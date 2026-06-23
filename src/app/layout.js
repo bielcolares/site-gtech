57fb2dfc1dcc84732d29dc81ae81465a02d35bd1
bielcolares <gabriel.colaresdossantos@gmail.com>
2026-06-23 00:30:47 -0300

    feat: adicionar Meta Pixel ao layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Script from 'next/script';
 import { Analytics } from '@vercel/analytics/react';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import { GoogleTagManager } from '@next/third-parties/google';
@@ -37,6 +38,30 @@ export default function RootLayout({ children }) {
           <WhatsAppModal />
         </LanguageProvider>
         <Analytics />
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}
+            (window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '317156430061443');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=317156430061443&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
       </body>
       <GoogleAnalytics gaId="G-J88G8H0RZY" />
       <GoogleTagManager gtmId="GTM-T7HQQSL9" />
